e9f1a71a200b0c84b06dba102489d95e082b8786
Charlie Fish <fishcharlie@me.com>
2016-07-14 15:34:44 -0600

    Homework Day 4

--- starter-code/pizza_shop/app.rb
+++ starter-code/pizza_shop/app.rb
@@ -1,2 +1,36 @@
+require 'json'
+require './models/pizza.rb'
+
 class PizzaShop < Sinatra::Base
+
+	get '/pizzas' do
+		Pizza.all.to_json
+	end
+
+	post '/pizzas' do
+		myPizza = JSON.parse(request.body.read)
+		pizza = Pizza.create(myPizza)
+		{"Done": "Done"}.to_json
+	end
+
+
+	get '/pizzas/:id' do
+		Pizza.find([params['id'].to_i]).to_json
+	end
+
+	delete '/pizzas/:id' do
+		Pizza.destroy([params['id'].to_i])
+		{"Done": "Done"}.to_json
+	end
+
+	put '/pizzas/:id' do
+		myPizza = JSON.parse(request.body.read)
+		pizza = Pizza.find(params['id'].to_i)
+		pizza.update_attributes(myPizza)
+		pizza.to_json
+		# myPizza.to_json
+	end
+
+
+
 end
